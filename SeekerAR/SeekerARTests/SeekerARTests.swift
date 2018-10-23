795e4b460cb7a87dd35373a9287d147bc76f67d2
Akitaka Yamashita <e165751@ie.uryukyu.ac.jp>
2018-10-23 15:46:09 +0900

    AR first

---
+++ SeekerAR/SeekerARTests/SeekerARTests.swift
@@ -0,0 +1,34 @@
+//
+//  SeekerARTests.swift
+//  SeekerARTests
+//
+//  Created by 山下　耀崇 on 2018/10/23.
+//  Copyright © 2018年 山下　耀崇. All rights reserved.
+//
+
+import XCTest
+@testable import SeekerAR
+
+class SeekerARTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
